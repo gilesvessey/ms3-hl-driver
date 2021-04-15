5819071643d481aa91ad6b230b96d09b02abe62c
gilesvessey <gilesvessey@gmail.com>
2021-04-14 23:33:05 -0300

    capture wip

--- ms3-hl-driver.ino
+++ ms3-hl-driver.ino
@@ -1,6 +1,6 @@
 #include <FastLED.h>
 #define LED_PIN     1
-#define NUM_LEDS    44
+#define NUM_LEDS    43
 CRGB leds[NUM_LEDS];
 
 void setup() {
@@ -25,7 +25,19 @@ void solid_colour_mode(struct rgb colour) {
   FastLED.show();
 }
 
-void liquid_fill_mode(struct rgb colour) {
+void pulse_mode(struct rgb colour, int frequency, double speed) {
+  bool brightening = false;
+  struct rgb curr = colour;
+  while(true) {
+    for (int led = 0; led < NUM_LEDS; led++) {
+
+    }
+  }
+}
+
+void liquid_fill_mode(struct rgb colour, double speed) {
+  // TODO adjustable drip size, animation takes a long time and will only take longer with 144/m
+  //
   int pool = 0;
   int drip = NUM_LEDS;
 
@@ -42,7 +54,7 @@ void liquid_fill_mode(struct rgb colour) {
         //
         leds[led] = CRGB(colour.r, colour.g, colour.b);
       } else {
-        
+
         // otherwise a blank pixel
         //
         leds[led] = CRGB(0, 0, 0);
@@ -71,9 +83,66 @@ void liquid_fill_mode(struct rgb colour) {
     }
 
     // wait before we do it again
+    // range is 5 - 15
     //
-    delay(8);     
+    delay(5 + (10 * speed));     
   }
+}
+
+void bounce_mode(struct rgb colour, bool isRainbow, double speed, int frequency) {
+  int ball = 0;
+  bool rising;
+  while(true) {
+    if (isRainbow) { 
+      for (int i = 0; i < frequency; i++) { colour = rainbow(colour); } 
+    }
+    for (int led = 0; led < NUM_LEDS; led++) {
+      if (led == ball) {
+        leds[led] = CRGB(colour.r, colour.g, colour.b);
+      } else {
+        leds[led] = CRGB(0, 0, 0);
+      }
+    }
+      
+      // change the leds
+      //
+      FastLED.show();
+
+      // do bouncy ball things
+      //
+      if (ball == 0) { rising = true; }
+      if (ball == NUM_LEDS) { rising = false; }
+
+      if (rising) { ball++; }
+      else { ball--; }
+
+    // wait before we do it again
+    // range is 20 - 200ms
+    //
+    delay(20 + (180 * speed));  
+  }
+}
+
+struct rgb pulse(struct rgb colour, int frequency, struct rgb curr, bool brightening) {
+  // frequency of 8 is 8/255ths change each tick
+  //
+  double multiplier = 1 + (frequency / 255);
+  if (brightening) {
+    curr.r = curr.r * multiplier; 
+    if (curr.r > colour.r) { curr.r = colour.r; }
+    curr.g = curr.g * multiplier; 
+    if (curr.g > colour.g) { curr.g = colour.g; }    
+    curr.b = curr.b * multiplier; 
+    if (curr.b > colour.b) { curr.b = colour.b; }      
+  } else {
+    curr.r = curr.r / multiplier; 
+    if (curr.r < colour.r) { curr.r = colour.r; }
+    curr.g = curr.g / multiplier; 
+    if (curr.g < colour.g) { curr.g = colour.g; }    
+    curr.b = curr.b / multiplier; 
+    if (curr.b < colour.b) { curr.b = colour.b; }  
+  }
+  return curr;
 }
 
 struct rgb rainbow(struct rgb value) {
@@ -101,7 +170,7 @@ struct rgb rainbow(struct rgb value) {
   }
 } 
 
-void rainbow_mode(int frequency, int change_per_tick) { 
+void rainbow_mode(double speed, int frequency) { 
   struct rgb start = { 255, 0, 0 };
   while (true) {
     struct rgb colour = start;
@@ -110,7 +179,6 @@ void rainbow_mode(int frequency, int change_per_tick) {
 
       // this is how tightly packed the rainbow will be
       //
-      int frequency = 8;
       for (int i = 0; i < frequency; i++) { colour = rainbow(colour); }
     }
     
@@ -119,8 +187,9 @@ void rainbow_mode(int frequency, int change_per_tick) {
     FastLED.show();
       
     // progress each led in the rainbow sequence for the next update
+    // range is 4 to 12
     //
-    int change_per_tick = 8;
+    int change_per_tick = 12 - (8 * speed);
     for (int i = 0; i < change_per_tick; i++) { start = rainbow(start); }
     
     // wait before we do it again
@@ -130,16 +199,13 @@ void rainbow_mode(int frequency, int change_per_tick) {
 }
 
 void loop() {
-  int frequency = 8;
-  int change_per_tick = 8;
-
-  struct rgb colour_1 = { 255, 0, 255 };
+  struct rgb colour_1 = { 255, 255, 255 };
 
   struct rgb colour_2 = { 255, 0, 255 };
 
   // logic for changing modes on the fly will come later
   //
-  int mode = 2;
+  int mode = 0;
 
   switch (mode) {
     case 0:
@@ -151,10 +217,11 @@ void loop() {
       break;
     case 2:
       // mode 2 - single colour liquid fill
-      liquid_fill_mode(colour_1);
+      liquid_fill_mode(colour_1, 0.5);
       break;
     case 3:
-      // mode 3 - single colour bouncing back and forth (alternating colour maybe?)
+      // mode 3 - bouncing back and forth
+      bounce_mode(colour_1, true, 0.5, 8);
       break;    
     case 4:
       // mode 4 - dual colour snake
@@ -167,7 +234,7 @@ void loop() {
       break;      
     case 7:
       // mode 6 - rainbow, baby
-      rainbow_mode(frequency, change_per_tick);
+      rainbow_mode(0.5, 8);
       break;      
   }
 }
